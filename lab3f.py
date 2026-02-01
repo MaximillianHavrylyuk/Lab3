6f2aca3d1c15becddfc185d4f63e53e67e1934d0
MaximillianHavrylyuk <havrmax@yahoo.ca>
2026-02-01 17:02:32 -0500

    Update lab3f.py

--- lab3f.py
+++ lab3f.py
@@ -2,14 +2,24 @@
 #Lab3f.py
 #exploring list functions and 2 dimension table usage
 
-#creating a new list calle list4
-list4 = [1,2,3,4,5]
-#creating a user input and using the append() function to store it in the list
-listadd = int(input("enter a number to add to list: "))
-list4.append(listadd)
-#multiplying every number by 10 after the append() from the user input so every number is multiplied by ten
-list4 = [x * 10 for x in list4]
-#sorting the list4 in descending order and printing the result
-for p in list4:
-    list4.sort(reverse=True)
-print(list4)
+#copied code from lab which is 3 lists in a 2 dimensional table
+matrix = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+]
+#returning the element from row 2 and column 3 into the variable element and printing the result
+element = matrix[1][2]
+print(element)
+#returning the element from the 2nd row and 2nd column and printing the result
+element1 = matrix[1][1]
+print(element1)
+#returning the element and printing it from the indexes 0 and 1
+element2 = matrix[0][1]
+print(element2)
+#returning and printing the element from the 3rd row and column
+element3 = matrix[2][2]
+print(element3)
+#printing the whole 2 dimensional table using a for loop
+for l in matrix:
+    print(l)
