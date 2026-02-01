ebcd7c704b7db37d7545c42486b84a31a572d6aa
MaximillianHavrylyuk <havrmax@yahoo.ca>
2026-02-01 17:01:26 -0500

    Update lab3e.py

--- lab3e.py
+++ lab3e.py
@@ -2,24 +2,14 @@
 #Lab3e.py
 #exploring list functions and 2 dimension table usage
 
-#copied code from lab which is 3 lists in a 2 dimensional table
-matrix = [
-[1, 2, 3],
-[4, 5, 6],
-[7, 8, 9]
-]
-#returning the element from row 2 and column 3 into the variable element and printing the result
-element = matrix[1][2]
-print(element)
-#returning the element from the 2nd row and 2nd column and printing the result
-element1 = matrix[1][1]
-print(element1)
-#returning the element and printing it from the indexes 0 and 1
-element2 = matrix[0][1]
-print(element2)
-#returning and printing the element from the 3rd row and column
-element3 = matrix[2][2]
-print(element3)
-#printing the whole 2 dimensional table using a for loop
-for l in matrix:
-    print(l)
+#Maximillian Havrylyuk
+#Lab3g.py
+#exploring list functions and 2 dimension table usage
+
+#cretaing a list for student names
+students = ["Ama", "Elina", "Maija", "Daniel", "Ibrahim"]
+#modifying an element in the student table at index 1. The element is changed to Maggy
+students[1] = "Maggy"
+#printing each name in the students list on a seperate line.
+for s in students:
+    print(s)
