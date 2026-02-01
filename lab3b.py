c1af45500d0d3a50b1bcf2bb65baca5c24433d95
MaximillianHavrylyuk <havrmax@yahoo.ca>
2026-02-01 16:48:06 -0500

    Create lab3b.py

---
+++ lab3b.py
@@ -0,0 +1,9 @@
+#Maximillian Havrylyuk
+#Lab3b.py
+#exploring list functions and 2 dimension table usage
+
+#making new list with random values picked by me
+list_b = [1,4,3,4,6,9,8,9]
+#reversing the elements so that it starts with 9,8,9 instead of 1,4,3 and printing the outcome
+list_b.reverse()
+print(list_b)
