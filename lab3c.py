5e7e23da940c7708a474d7828632ec952842107a
MaximillianHavrylyuk <havrmax@yahoo.ca>
2026-02-01 16:48:42 -0500

    Create lab3c.py

---
+++ lab3c.py
@@ -0,0 +1,10 @@
+#Maximillian Havrylyuk
+#Lab3c.py
+#exploring list functions and 2 dimension table usage
+
+#creating two lists with set elements
+mylist1 = [1,3,5]
+mylist2 = [0,2,4]
+#concating the lists into one variable which will make it one big list and printing the outcome
+mylist = mylist1 + mylist2
+print(mylist)
